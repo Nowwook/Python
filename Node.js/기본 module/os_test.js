a18123be6e87689c6323363818d71322aadb0f63
Nowwook <110366684+Nowwook@users.noreply.github.com>
2022-09-17 23:24:09 +0900

    Create os_test.js

---
+++ Node.js/기본 module/os_test.js
@@ -0,0 +1,12 @@
+const os =require('os')
+
+console.log(os.arch())      // cpu 아키텍처
+console.log(os.cpus())
+console.log(os.hostname())
+console.log(os.networkInterfaces())
+console.log(os.type())      // 운영체제 타입
+console.log(os.release())
+console.log(os.homedir())
+console.log(os.tmpdir())
+console.log(os.totalmem())
+console.log(os.freemem())
