9e39514917af0c755dd76b099d6c459ffcd3bacc
Nowwook <110366684+Nowwook@users.noreply.github.com>
2022-09-23 23:05:02 +0900

    Add files via upload

--- Node.js/기본 module/timer.js
+++ Node.js/기본 module/timer.js
@@ -1,19 +1,12 @@
-const timeout =setTimeout(() => {
-    console.log('1초 후 실행')
-}, 1000)
+const fs = require('fs')
 
 const interval = setInterval(() => {
-    console.log('1초 마다 실행')
+    let time = new Date()
+    fs.appendFileSync("currentTime.txt",time.toLocaleTimeString(),{encoding: 'utf-8'})
+    console.log(time.toLocaleTimeString())
 },1000)
 
-const immediate = setImmediate(() => {
-    console.log('setImmediate() 함수 호출 뒤에 오는 모든 코드를 먼저 실행')
-})
-
-console.log('먼저 실행')
-
-// 변수명이 interval인 1초마다 콘솔창에 출력
 setTimeout(() => {
     clearInterval(interval)
-    // setinterval()함수를 종료
-},3000)
+},6000)
+
