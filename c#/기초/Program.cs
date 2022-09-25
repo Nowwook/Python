29366f2db02c6e6a0c520949ff05dc827043154c
Nowwook <110366684+Nowwook@users.noreply.github.com>
2022-09-25 23:32:15 +0900

    Add files via upload

---
+++ c#/기초/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp005
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch = 'A';
+            char ch1 = '\t'; // 4칸 띄우기
+            char ch2 = '\n'; //엔터
+            char ch3 = 'o';
+
+            string str1 = "\tABC\n";
+
+            Console.WriteLine(ch);
+            Console.WriteLine(ch1);
+            Console.WriteLine(ch2);
+            Console.WriteLine(ch3);
+            Console.WriteLine(str1);
+        }
+    }
+}
