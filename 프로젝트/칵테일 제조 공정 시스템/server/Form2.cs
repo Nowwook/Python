220d43d667d0b7892f3ccf608957f764edca5593
Nowwook <110366684+Nowwook@users.noreply.github.com>
2022-09-30 17:32:27 +0900

    Update Form2.cs

--- 프로젝트/칵테일 제조 공정 시스템/server/Form2.cs
+++ 프로젝트/칵테일 제조 공정 시스템/server/Form2.cs
@@ -18,10 +18,6 @@ namespace server123
             InitializeComponent();
         }
 
-        private void Form2_Load(object sender, EventArgs e)
-        {
-        }
-
         private void button1_Click(object sender, EventArgs e)
         {
             string constring = 
