38771455df328d4ac2a59f8c66b02d5ae11ac474
Nowwook <110366684+Nowwook@users.noreply.github.com>
2022-10-17 01:54:42 +0900

    Create raspberrypi_sound.py

---
+++ 프로젝트/조류 퇴치 시스템/raspberrypi_sound.py
@@ -0,0 +1,20 @@
+import time
+import pygame as p
+
+# 다운 sudo apt-get install python3-pygame
+  
+p.mixer.init()
+# 모듈 초기화
+
+# -1 무한 ,2 2번
+
+# 음향 조절 0~1
+# p.mixer.music.set_volume(1)
+
+while True:
+    p.mixer.music.load( "2.mp3" )
+    p.mixer.music.play(2)
+    time.sleep(15)
+    p.mixer.music.load( "1.mp3" )
+    p.mixer.music.play(2)
+    time.sleep(15)
