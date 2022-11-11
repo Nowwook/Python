cdf0880010cd9af10da797abc7441097781a1010
Nowwook <110366684+Nowwook@users.noreply.github.com>
2022-11-11 10:35:39 +0900

    Create python_type.py

---
+++ Python/python_type.py
@@ -0,0 +1,40 @@
+# 데이터 타입
+v_str1 = "Niceman"    # str
+v_str2 = "Goodgirl"   # str
+
+v_bool = True     # bool
+
+v_float = 10.3    # float
+v_int = 7         # int
+
+v_complex = 3 + 3j  # complex
+
+v_dict = {
+    "name" : "Jang",
+    "age" : 32,
+    "city" : "seoul"
+}                     # dict
+
+v_list = [3,5,7]      # list
+
+v_tuple = 3,5,7       # tuple
+
+v_set = {7,8,9}       # set
+
+print(type(v_str1))     # <class 'str'>
+print(type(v_str2))     # <class 'str'>
+
+print(type(v_bool))     # <class 'bool'>
+
+print(type(v_float))    # <class 'float'>
+print(type(v_int))      # <class 'int'>
+
+print(type(v_complex))  # <class 'complex'>
+
+print(type(v_dict))     # <class 'dict'> 
+
+print(type(v_list))     # <class 'list'>
+
+print(type(v_tuple))    # <class 'tuple'>
+
+print(type(v_set))      # <class 'set'>
