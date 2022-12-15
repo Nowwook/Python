484ebc8a38f2e21359f7a2b9516a4bd4eecc07b0
Nowwook <110366684+Nowwook@users.noreply.github.com>
2022-12-16 00:04:31 +0900

    Update and rename cv2_threshold.py to cv2_threshold.txt

--- OpenCV/cv2_threshold.py
+++
@@ -1,6 +0,0 @@
-# img = 이미지, gray로 변환
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-ret, thresh = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY)
-
-# cv2.threshold(a,b,c,cv2.THRESH_BINARY)
-# a 그림에서 b보다 크면c 나머진0
